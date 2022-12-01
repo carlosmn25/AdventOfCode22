14b96a58bdb33be0b8c63e55366013668ce6d9b3
cmedrano <carlosvalon75@gmail.com>
2022-12-02 00:22:57 +0100

    Day 1 exercise 2 solved

--- day1.py
+++ day1.py
@@ -5,6 +5,7 @@ lines = input.readlines();
 
 max = 0;
 sum = 0;
+calories_list = [];
 
 for l in lines:
     if (l != "\n"):
@@ -14,7 +15,10 @@ for l in lines:
         if(sum > max):
             max = sum
     else:
+        calories_list.append(sum) #store last result and reset
         sum = 0;
 
 input.close();
-print("The max number of calories is " + str(max))
+calories_list = sorted(calories_list, reverse=True)
+print("The Top 3 calories are {t1}, {t2} and {t3}!!!".format(t1=calories_list[0], t2=calories_list[1], t3=calories_list[2]))
+print("The addition of Top 3 is: {add}".format(add=(calories_list[0] + calories_list[1] + calories_list[2])))
