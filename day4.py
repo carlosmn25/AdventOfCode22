ddb48040304f28b620bafbc8bdba6d56cecce0e5
cmedrano <carlosvalon75@gmail.com>
2022-12-05 00:44:55 +0100

    Day 4 exercise 2 solved!

--- day4.py
+++ day4.py
@@ -1,8 +1,13 @@
-def ranges_overlap(r1start,r1end,r2start,r2end):
+def ranges_overlap(r1start,r1end,r2start,r2end): #Exercise 1
     if((r1start >= r2start and r1end <= r2end) or (r2start >= r1start and r2end <= r1end)): return True
-    """ if((r1start >= r2start) and (r1end <= r2end)): return True
-    elif (r2start >= r1start and r2end <= r1end): return True """
     return False
+
+def any_overlapping(r1start,r1end,r2start,r2end): #Exercise 2
+    if(r1start in range(r2start,r2end+1) or r1end in range(r2start,r2end+1) or r2start in range(r1start,r1end+1) or r2end in range(r1start,r1end+1)):
+        return True
+    return False
+
+
 
 input = open("input4.txt", "r")
 lines = input.readlines();
@@ -14,6 +19,6 @@ for l in lines:
     limits = []
     for r in ranges:
         limits.append(r.split('-'))
-    if(ranges_overlap(int(limits[0][0]),int(limits[0][1]),int(limits[1][0]),int(limits[1][1]))): sum += 1
+    if(any_overlapping(int(limits[0][0]),int(limits[0][1]),int(limits[1][0]),int(limits[1][1]))): sum += 1
 
 print(sum)
