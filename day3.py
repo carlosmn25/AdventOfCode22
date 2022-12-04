6bbc63546522a297850f663b7385721e269503d3
cmedrano <carlosvalon75@gmail.com>
2022-12-04 23:52:53 +0100

    Day 3 exercise 2 solved

--- day3.py
+++ day3.py
@@ -9,24 +9,49 @@ def find_repeated(comp1, comp2):
         if(c in appears_in_comp1): #Return the first character that appears in both halves
             return c
 
+def find_badge(group_list): #Receives 3 lines and returns the only character that appears in all of them
+    countdict = dict();
+    for group in group_list:
+        gindex = group_list.index(group);
+        for character in group:
+            if(character == '\n'): continue #ignore line breaks
+            if(character not in countdict):
+                countdict[character] = [False,False,False]
+            countdict[character][gindex] = True
+    
+    for entry in countdict:
+        if(countdict[entry] == [True,True,True]): return entry
+
+
 alphabet = list(string.ascii_letters); #generate alphabet
 priorities = dict();
 for i in range(0,52):
     priorities[alphabet[i]] = i + 1 #fill priorities dict
 
+#input = open("input3.txt", "r")
 input = open("input3.txt", "r")
-#input = open("test3.txt", "r")
 lines = input.readlines();
 sum = 0
 
-for line in lines:
+""" for line in lines:
     length = len(line)
     split_index = int((length/2))
     first = line[0:split_index]
     second = line[split_index:length]
     rep = find_repeated(first,second)
-    sum += priorities[rep]
+    sum += priorities[rep] """
 
-print("======================")
-print(sum)
-print(len(lines))
+group = 0;
+group_list = []
+for line in lines:
+    if (group == int(lines.index(line)/3)): #Get group id
+        group_list.append(line)
+        if(int(lines.index(line)%3 == 2)):
+            badge = find_badge(group_list) #Get the badge for the current group
+            sum += priorities[badge]
+    else:
+        group_list = []
+        group_list.append(line)
+        group += 1;
+
+print('The sum of all priorities is: ', sum)
