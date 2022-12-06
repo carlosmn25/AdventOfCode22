2be147cf826c5f5dff81f95fdddd916b7f77efa9
cmedrano <carlosvalon75@gmail.com>
2022-12-07 00:22:13 +0100

    Day 6 exercise 1 solved

---
+++ day6.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import queue
+elements = queue.Queue()
+input = open("input6.txt", "r")
+text = input.readline()
+#text = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
+index = 0
+
+for char in text:
+    if(char in elements.queue):
+        while char in elements.queue:
+            elements.get() #Remove the first elements in the queue until the repeated one doesn't appear
+        elements.put(char)
+    elif(len(elements.queue) == 3): #Not found and it's the 4th char
+        print(index + 1) #print the index of the character
+        break
+    else:
+        elements.put(char);
+    
+    index+=1
